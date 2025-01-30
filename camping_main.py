c5d7def57fe0212e512f7a74c52b16816feda063
Dataloops-code <dataloopskw.code@gmail.com>
2025-01-30 06:31:20 +0200

    Update camping_main.py

--- camping_main.py
+++ camping_main.py
@@ -7,7 +7,7 @@ from datetime import datetime, timedelta
 from typing import Dict, List, Tuple
 from pathlib import Path
 from DetailsScraper import DetailsScraping
-from SavingOnDrive import SavingOnDrive
+from SavingOnDriveCamping import SavingOnDriveCamping
 
 
 class CampingMainScraper:
@@ -138,7 +138,7 @@ class CampingMainScraper:
                 self.logger.info("Environment variable CAMPINGS_GCLOUD_KEY_JSON is set.")
 
             credentials_dict = json.loads(credentials_json)
-            drive_saver = SavingOnDrive(credentials_dict)
+            drive_saver = SavingOnDriveCamping(credentials_dict)
             drive_saver.authenticate()
             self.logger.info("Testing Drive API access...")
             try:
